7ded962ac5e99117ad1d1245c7da46f6105f5c5c
Oscar Reyes <o.scar_r@yahoo.com.mx>
2016-10-11 11:02:59 -0500

    Created the Task class and added its properties

---
+++ src/ch/makery/task/model/Task.java
@@ -0,0 +1,10 @@
+package ch.makery.task.model;
+
+public class Task {
+	private String taskName;
+	private String description;
+	private String dueDate;
+	private String location;
+	private char priority;
+	private char status;
+}
