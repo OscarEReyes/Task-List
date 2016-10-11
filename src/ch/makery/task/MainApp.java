a08a90266e7849f842a30ca7e082fe8788f2f51b
Oscar Reyes <o.scar_r@yahoo.com.mx>
2016-10-11 10:59:18 -0500

    Added Main java class, created fxml file for root layout.

---
+++ src/ch/makery/task/MainApp.java
@@ -0,0 +1,70 @@
+package ch.makery.task;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	 private Stage primaryStage;
+	 private BorderPane rootLayout;
+	    
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Task List");
+
+        initRootLayout();
+
+        showTaskOverview();
+	}
+	
+	public void initRootLayout(){
+		  try {
+	          // Load the root layout from view/RootLayout.fxml file.
+	          FXMLLoader loader = new FXMLLoader();
+	          loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+	          rootLayout = (BorderPane) loader.load();
+	
+	          // Set scene to the scene containing the root layout.
+	          Scene scene = new Scene(rootLayout);
+	          primaryStage.setScene(scene);
+	          primaryStage.show();
+	          
+	      } catch (IOException e) {
+	          e.printStackTrace();
+	      }
+	
+	}
+
+	public void showTaskOverview() {
+	    try {
+	        // Load task overview.
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(MainApp.class.getResource("view/TaskOverview.fxml"));
+	        AnchorPane taskOverview = (AnchorPane) loader.load();
+	
+	        // Center task overview in root layout
+	        rootLayout.setCenter(taskOverview);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	 /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+ 
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
