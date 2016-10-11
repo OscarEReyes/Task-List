1919f797083dbb3e05c5e896d8fae619b9a5e674
Oscar Reyes <o.scar_r@yahoo.com.mx>
2016-10-11 11:44:09 -0500

    Added TaskOverviewController class

---
+++ src/tasklist/task/view/TaskOverviewController.java
@@ -0,0 +1,12 @@
+package tasklist.task.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import tasklist.task.MainApp;
+import tasklist.task.model.Task;
+
+public class TaskOverviewController {
+	
+}
